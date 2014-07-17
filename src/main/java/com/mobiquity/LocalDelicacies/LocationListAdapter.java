ac875b6caae7a10f8eadafc2be2f6b6347b9994d
Corey Leigh Latislaw <colabug@gmail.com>
2014-07-17 16:53:31 -0400

    REFACTOR: Whitespace

--- src/main/java/com/mobiquity/LocalDelicacies/LocationListAdapter.java
+++ src/main/java/com/mobiquity/LocalDelicacies/LocationListAdapter.java
@@ -10,33 +10,39 @@ import java.util.ArrayList;
 /**
  * Created by jwashington on 7/17/14.
  */
-public class LocationListAdapter extends BaseAdapter {
-    private Context theContext;
+public class LocationListAdapter extends BaseAdapter
+{
+    private Context           theContext;
     private ArrayList<String> locationList;
 
-    public LocationListAdapter(Context theContext, ArrayList<String> locationList)
+    public LocationListAdapter( Context theContext,
+                                ArrayList<String> locationList )
     {
         this.theContext = theContext;
         this.locationList = locationList;
     }
 
     @Override
-    public int getCount() {
+    public int getCount()
+    {
         return locationList.size();
     }
 
     @Override
-    public Object getItem(int position) {
-        return locationList.get(position);
+    public Object getItem( int position )
+    {
+        return locationList.get( position );
     }
 
     @Override
-    public long getItemId(int position) {
+    public long getItemId( int position )
+    {
         return position;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView( int i, View view, ViewGroup viewGroup )
+    {
         return null;
     }
 }
